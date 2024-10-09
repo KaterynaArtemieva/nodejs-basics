24fc738afd804d1cbfd4f04d1fe3d39ad6545cd1
KaterynArtemieva <moruga.katya@gmail.com>
2024-10-09 21:50:55 +0100

    3.1done

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getAllStudents, getStudentById } from './services/students.js';
 
 import { env } from './utils/env.js';
+import studentsRouter from './routers/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
 
   app.use(express.json());
   app.use(cors());
@@ -30,40 +29,13 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(studentsRouter);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
-  
-  app.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-    const student = await getStudentById(studentId);   
-    
-	if (!student) {
-	  res.status(404).json({
-		  message: 'Student not found'
-	  });
-	  return;
-	}
+  app.use('*', notFoundHandler);
 
-    res.status(200).json({
-      data: student,
-    });
-  });
+  app.use(errorHandler);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 };
